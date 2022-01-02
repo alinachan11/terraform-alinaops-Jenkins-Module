4d5928182ee83457fe9ed7953fa6ce4b1c29e663
alinachan11 <alinachan460@gmail.com>
2022-01-02 21:47:46 +0200

    added a tag

--- user_data.tf
+++ user_data.tf
@@ -7,14 +7,6 @@ service docker start
 usermod -aG docker ec2-user
 USERDATA
 
-jenkins-server-userdata11 = <<USERDATA
-#!/bin/bash 
-
-chmod 600 /home/ubuntu/${var.keypath}
-mv /home/ubuntu/${var.keypath} /home/ubuntu/.ssh/
-
-USERDATA
-
 jenkins-server-userdata = <<USERDATA
 #!/bin/bash 
 
@@ -22,5 +14,6 @@ chmod 600 /home/ubuntu/${var.keypath}
 mv /home/ubuntu/${var.keypath} /home/ubuntu/.ssh/
 
 docker run -d --restart=always -p 8080:8080 -p 50000:50000 -v ${local.jenkins_home_mount} -v ${local.docker_sock_mount} --env ${local.java_opts} jenkins/jenkins
+
 USERDATA
 }
